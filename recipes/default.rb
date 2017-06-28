7a233e7e2672aa0712d1d28414c10ec1eab59ca4
Nolan Davidson <ndavidson@chef.io>
2017-06-28 11:35:17 -0400

    initial commit
    Signed-off-by: Nolan Davidson <ndavidson@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'apt::default'
+
+if node.platform_family? 'rhel'
+  apache_name = 'httpd'
+else
+  apache_name = 'apache2'
+  package 'curl'
+end
+
+package apache_name do
+  action [:install]
+end
+
+service apache_name do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  variables({
+    :message => node['aikcu-demo']['message']
+  })
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,15 +4,24 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+if os.redhat?
+  apache_name = 'httpd'
+  port = '8080'
+else
+  apache_name = 'apache2'
+  port = '8081'
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package(apache_name) do
+  it { should be_installed }
+end
+
+describe service(apache_name) do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe http("http://localhost:#{port}") do
+  its('status') { should cmp 200 }
+  its('body') { should include 'Hello world!' }
 end
